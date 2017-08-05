ebec01002f5a752b3a06f059e3784fa310ea6b27
rrhegganni <rrhegganni@gmail.com>
2017-08-05 16:37:38 +0530

    added express

--- express/express-tutorials/index.js
+++ express/express-tutorials/index.js
@@ -4,19 +4,37 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser());
 
+app.use(express.static('images'));
 
-//app.METHOD(path, ...handler(middlewares))
+var middlewares = function(req, res, next) {
+  console.log("Great I'm a middleware");
+  if(req.params) {
+    res.send()
+  } else {
+    next();
+  }
+}
 
-app.get('/', function(req, res) {
+var errHandler = function(err, req, res, next) {
+  if(err) {
+    console.log("ERROR: ", err);
+  }
+}
 
-  res.send("WELCOME TO EXPRESS");
+app.use(errHandler);
+
+app.get('/', middlewares, function(req, res) {
+  throw new Error("Something went wrong");
+  console.log("callback function");
+  res.send()
 });
 
 app.post('/', function(req, res) {
-  app.use(bodyParser());
   var body = req.body;
   res.send("Great I got this:-> " + JSON.stringify(body));
 });
+
+
 
 app.listen(3000, function(err) {
   if(err) console.log(err);
